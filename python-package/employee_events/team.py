fa7a0a0afdba4ca750fb7e37ca580c89c1828d4b
Saad711T <75455552+Saad711T@users.noreply.github.com>
2025-03-18 06:12:01 +0300

    -

--- python-package/employee_events/team.py
+++ python-package/employee_events/team.py
@@ -1,38 +1,42 @@
 # Import the QueryBase class
 # YOUR CODE HERE
-
+from .query_base import QueryBase
 # Import dependencies for sql execution
 #### YOUR CODE HERE
-
+from .sql_execution import execute_sql, create_connection
+import pandas as pd
 # Create a subclass of QueryBase
 # called  `Team`
 #### YOUR CODE HERE
-
+class Team(QueryBase):
     # Set the class attribute `name`
     # to the string "team"
     #### YOUR CODE HERE
-
+    name = "team"
 
     # Define a `names` method
     # that receives no arguments
     # This method should return
     # a list of tuples from an sql execution
     #### YOUR CODE HERE
-        
+    def names(self):
         # Query 5
         # Write an SQL query that selects
         # the team_name and team_id columns
         # from the team table for all teams
         # in the database
         #### YOUR CODE HERE
-    
+        query = """SELECT team_id,
+        team_name FROM {self.name}"""
+        return self.query(query)
+
 
     # Define a `username` method
     # that receives an ID argument
     # This method should return
     # a list of tuples from an sql execution
     #### YOUR CODE HERE
-
+    def username(self, id):
         # Query 6
         # Write an SQL query
         # that selects the team_name column
@@ -40,6 +44,9 @@
         # to only return the team name related to
         # the ID argument
         #### YOUR CODE HERE
+        query = """SELECT team_name FROM {self.name} 
+        WHERE team_id = {id}"""
+        return self.query(query)
 
 
     # Below is method with an SQL query
@@ -51,8 +58,8 @@
     # the sql query
     #### YOUR CODE HERE
     def model_data(self, id):
-
-        return f"""
+        
+        query = f"""
             SELECT positive_events, negative_events FROM (
                     SELECT employee_id
                          , SUM(positive_events) positive_events
